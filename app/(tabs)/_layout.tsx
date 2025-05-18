291c12fc2267777eaa0c0bf018c72847332231a6
SeckMohameth <mohamethseck34@gmail.com>
2025-05-18 15:23:26 -0400

    mo part done

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,27 @@
+import { Ionicons } from "@expo/vector-icons";
+import { Tabs } from "expo-router";
+
+export default function TabLayout() {
+  return (
+    <Tabs>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Mood",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="happy-outline" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="explore"
+        options={{
+          title: "Explore",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="map-outline" size={size} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
